f744d803b2b5ed451378bbc7a81f3e85fab83b8a
YuriiTrach-bio <tracyuriy@gmail.com>
2022-09-10 18:15:21 +0300

    Binded data.

--- run_analysis.R
+++ run_analysis.R
@@ -1,10 +1,10 @@
 library(dplyr)
 
 ## Downloading the data ====
-url <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+#  url <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
 
-download.file(url = url, destfile =paste(getwd(), "/data.zip", sep="")) # download the archive
-unzip("data.zip") # extract a folder from the archive
+#  download.file(url = url, destfile =paste(getwd(), "/data.zip", sep="")) # download the archive
+#  unzip("data.zip") # extract a folder from the archive
 
 ## Reading data ====
 
@@ -25,13 +25,23 @@ y_train <- read.table("UCI HAR Dataset/train/y_train.txt")
                 names(X_test) <- features[,2]
                 names(X_train) <- features[,2]
         #y_test & y_traine
+                #Decoding of variables
+                y_test <- merge(y_test, activity_labels, by="V1", sort=FALSE) %>% 
+                        select(V2)
+                y_train <- merge(y_train, activity_labels, by="V1", sort=FALSE)%>% 
+                        select(V2)
                 #Rename variables
                 names(y_test) <- "test_activ"
                 names(y_train) <- "train_activ"
-                #Decoding of variables
-                
-                
-                
-                
+        #subject_test & subject_train
+                #Rename varibles
+                names(subject_train) <- "subject"
+                names(subject_test) <- "subject"
+## Data binding ====
+        test_bind <- cbind(y_test, subject_test, X_test)
+        train_bind <- cbind(y_train, subject_train, X_train)
+                # Addin group column
+                test_bind$Group <- "test"
+                train_bind$Group <- "train"
                 
                 
