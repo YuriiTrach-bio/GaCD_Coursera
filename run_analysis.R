3a2ad57c4a7bae7fe966872a7cbae3cd68c15ba9
YuriiTrach-bio <tracyuriy@gmail.com>
2022-09-10 16:52:21 +0300

    Renamed some varibles.

--- run_analysis.R
+++ run_analysis.R
@@ -1,3 +1,5 @@
+library(dplyr)
+
 ## Downloading the data ====
 url <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
 
@@ -7,6 +9,7 @@ unzip("data.zip") # extract a folder from the archive
 ## Reading data ====
 
 activity_labels <- read.table("UCI HAR Dataset/activity_labels.txt")
+features <-  read.table("UCI HAR Dataset/features.txt")
 
 subject_test <- read.table("UCI HAR Dataset/test/subject_test.txt")
 X_test <- read.table("UCI HAR Dataset/test/X_test.txt")
@@ -15,3 +18,20 @@ y_test <- read.table("UCI HAR Dataset/test/y_test.txt")
 subject_train <- read.table("UCI HAR Dataset/train/subject_train.txt")
 X_train <- read.table("UCI HAR Dataset/train/X_train.txt")
 y_train <- read.table("UCI HAR Dataset/train/y_train.txt")
+
+## Modifying the data ====
+        #X_test & X_traine
+                #Rename variables
+                names(X_test) <- features[,2]
+                names(X_train) <- features[,2]
+        #y_test & y_traine
+                #Rename variables
+                names(y_test) <- "test_activ"
+                names(y_train) <- "train_activ"
+                #Decoding of variables
+                
+                
+                
+                
+                
+                
